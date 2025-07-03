9ad807e89e5a181ce26468574907afe3eba05265
prokann02 <prokopovann02@gmail.com>
2025-07-03 17:48:34 +0300

    Fixed host getter

--- app/utils/target_getter/host_getter.py
+++ app/utils/target_getter/host_getter.py
@@ -1,6 +1,7 @@
 import logging
 
 from app.models.host_config import Host
+from app.utils.parsing_utils import is_substring
 
 
 async def get_host(
@@ -20,9 +21,13 @@ async def get_host(
             start_index = data.find("//")
             data = data[start_index + 2 if start_index != -1 else 0 :]
 
-            # TODO: Check if it is correct. Target AND IP address?
-            if data in host.target or data in host.ip_address:
+            if host.ip_address == data:
                 return host
+
+            # TODO: Check if it is correct. Target AND IP address?
+            for app in host.associated_applications:
+                if await is_substring(data, app.target):
+                    return host
 
     logger.warning(f"Host with data '{data}' not found in the provided hosts list.")
     return None
